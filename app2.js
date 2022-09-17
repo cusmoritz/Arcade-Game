35a5a48a54e79ac6d87f89e630c78fcde421eb4c
cusmoritz <cus.moritz@gmail.com>
2022-09-17 10:36:43 -0600

    have collision working with top and bottom

--- app2.js
+++ app2.js
@@ -18,7 +18,8 @@ snake: snake,
 score: 0,
 time: 0,
 applecounter: 0,
-gameRunning: startGame(false)
+gameRunning: startGame(false),
+size: 20,
 }
 
 // let ticker = window.setInterval(tick, 200);
@@ -27,11 +28,12 @@ function startGame(boolean) {
     if (boolean === true){ // if we click start, the tick function begins
         window.setInterval(tick, 200);
     } else { // if we click pause, the tick function should stop running
-        window.clearInterval(tick); // we will come back to this
+        window.clearInterval(); // we will come back to this
     }
 }
 
 function tick() {
+    collisionWalls();
     moveSnake(snake.nextDirection);
     appleEaten();
     gameState.time = gameState.time + 1;
@@ -63,10 +65,10 @@ function buildInitialState() {
 
     // create 20 rows with 20 cells each
 
-    for (let rowsInTable = 1; rowsInTable < 20; rowsInTable++){
+    for (let rowsInTable = 0; rowsInTable < gameState.size; rowsInTable++){
         let newRow = document.createElement('tr');
 
-        for (let cellsInRow = 1; cellsInRow < 20; cellsInRow++){
+        for (let cellsInRow = 0; cellsInRow < gameState.size; cellsInRow++){
             let newCell = document.createElement('td');
             
             newRow.appendChild(newCell);
@@ -116,7 +118,7 @@ function moveSnake(array){
     colorSnake();
 
     // check if the apple was eaten after each move
-    appleEaten();
+    // appleEaten();
 }
 
 function colorSnake (){ // styles each snake cell with "snake"
@@ -125,7 +127,6 @@ function colorSnake (){ // styles each snake cell with "snake"
     })
 
     removeTail();
-
 }
 
 function removeTail(){ // removes "snake" class from first 'snake' array elem.
@@ -139,8 +140,8 @@ function drawApple() {
     gameState.eaten = false;
 
     //random apple row and cell
-    let randomRow = Math.ceil(Math.random() * newGameBoard.children.length);
-    let newAppleCell = Math.ceil(Math.random() * newGameBoard.children[0].children.length);
+    let randomRow = Math.floor(Math.random() * newGameBoard.children.length);
+    let newAppleCell = Math.floor(Math.random() * newGameBoard.children[0].children.length);
 
     // put random row and cell in apple array
     gameState.apple[0] = randomRow;
@@ -177,13 +178,13 @@ function appleEaten(){
         // and adds 5 to the score if we eat an apple
         gameState.score = gameState.score + 5;
 
-        console.log('score', gameState.score);
+        // console.log('score', gameState.score);
 
         addTail();
 
         drawApple();
 
-        console.log(snake.body.length);
+        // console.log(snake.body.length);
 
         // we also need to make the snake array 1 cell longer
             // push current coordinates of snake tail into snake.body array
@@ -257,9 +258,62 @@ function scoreCounter(){
 }
 
 function addTail(){
-    snake.body.unshift(snake.body[0])
+    snake.body.unshift(snake.body[0]);
 }
 
 function gameOver() {
-    alert(`Good game! Your final score was ${gameState.score}, you ate ${gameState.applecounter} apples, and you survived for ${gameState.time / 4} seconds`);
+    alert(`Good game! Your final score was ${gameState.score}, you ate ${gameState.applecounter} apples, and you survived for ${Math.trunc(gameState.time / 6)} seconds`);
+
+    startGame(false);
 }
+
+function collisionWalls() {
+    // if snake head array coord = rest of snake array
+        // gameOver()
+    // else
+        // return
+    let snakeHead = (snake.body[snake.body.length - 1]);
+    console.log(snakeHead);
+    // let snakeHead0 = snakeHead[0];
+    // let snakeHead1 = snakeHead[1];
+    // snake.body.forEach(element =>
+
+
+    // console.log(lastRow);
+    // check the snakehead coordinates ,
+        // if they go beyond the bounds of the game board
+            // game over
+        // else
+            // return
+
+    // let snake = {
+    //     body: [ [10, 5], [10, 6], [10, 7], [10, 8] ],
+
+    if ((snakeHead[0] < 0) || (snakeHead[0] > gameState.size)){
+        gameOver(); // check to make sure the snake is inside the game board width
+        console.log('ded height');
+    } else {
+        return;
+    }
+
+    if ((snakeHead[1] < 0) || (snakeHead[1] > gameState.size)){
+        gameOver(); // check to make sure snake is within height of game
+        console.log('ded width');
+    } else {
+        return;
+    }
+}
+
+// function collisionApple(){
+    // for (let i = 0; i < snake.body.length; i++){
+    //     if (snakeHead[0] === snake.body[i][0] && snakeHead[1] === snake.body[i][1])
+    //         gameOver();
+    //         console.log('collision at ', snake.body[i]);
+    //     } else {
+    //         // console.log('snake body i', snake.body[i]);
+    //         // console.log('snakehead 0 ', snakeHead[0]);
+    //         // console.log('snakehead 1 ', snakeHead[1]);
+    //         return;
+    //     }
+    // }
+// }
